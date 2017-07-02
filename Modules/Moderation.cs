a03112fdf1b1eb6ab712e5d24b019f16f93d5953
Dingsi <Dingsi@users.noreply.github.com>
2017-07-03 00:26:48 +0200

    Added Moderation Module

---
+++ Modules/Moderation.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumpiBot.Modules
+{
+    public class Moderation : ModuleBase
+    {
+        [Command("kick")]
+        [Remarks("Kick the specified user.")]
+        [RequireUserPermission(Discord.GuildPermission.KickMembers)]
+        public async Task Kick([Remainder]SocketGuildUser user)
+        {
+            await ReplyAsync($"Cya {user.Mention} :wave:");
+            await user.KickAsync();
+        }
+    }
+}
